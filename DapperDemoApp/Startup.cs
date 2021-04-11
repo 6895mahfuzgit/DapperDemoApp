f1a1994f3d5aeb67a36c2b9489c28cfa447ad686
6895mahfuzgit <6895mahfuz@gmail.com>
2021-04-11 23:31:58 +0600

    Created Company Table in Database.

--- DapperDemoApp/Startup.cs
+++ DapperDemoApp/Startup.cs
@@ -1,6 +1,8 @@
+using DapperDemoApp.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,7 +25,9 @@ namespace DapperDemoApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDBContext>(opt=> opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
