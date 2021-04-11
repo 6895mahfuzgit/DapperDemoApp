41a35f138f0caf19cbd626e6fb8391680020716a
6895mahfuzgit <6895mahfuz@gmail.com>
2021-04-11 23:18:17 +0600

    Added Company Model and Connection string  in Appsettings.

---
+++ DapperDemoApp/Models/Company.cs
@@ -0,0 +1,16 @@
+﻿namespace DapperDemoApp.Models
+{
+    public class Company
+    {
+        
+        public int CompanyId { get; set; }
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+
+        public string PostCode { get; set; }
+
+    }
+}
